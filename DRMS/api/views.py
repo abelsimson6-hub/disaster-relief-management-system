734354257cc21dea87f5ebe6bb3ba081c125a25c
Krishnaja <krishnaja678@gmail.com>
2026-01-09 23:11:22 +0530

    Fix missing authenticate import in api/views.py

--- DRMS/api/views.py
+++ DRMS/api/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.conf import settings
 from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
