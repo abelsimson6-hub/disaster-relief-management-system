28f547d986be228eeb42a206b4b91e0c9d6a9ada
jithin B <jithinmb05@gmail.com>
2026-01-10 00:35:29 +0530

    Fix main.dart: correct imports and required constructor args

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:provider/provider.dart';
 import 'package:relief/app_state.dart';
 import 'package:relief/screens/splash_screen.dart';
 import 'package:relief/screens/welcome_screen.dart';
-import 'package:relief/screens/register_placeholder.dart';
+import 'package:relief/screens/register_screen.dart';
+import 'package:relief/screens/role_selection_screen.dart';
+import 'package:relief/screens/map_screen.dart';
+import 'package:relief/screens/donation_details.dart';
 import 'package:relief/screens/profile_screen.dart';
 import 'package:relief/screens/request_details.dart';
 import 'package:relief/screens/camp_admin_dashboard.dart';
@@ -40,25 +43,44 @@ class MyApp extends StatelessWidget {
   Widget _screenFor(AppState appState) {
     switch (appState.currentScreen) {
       case Screen.splash:
-        return const SplashScreen();
+        return SplashScreen(onComplete: () => appState.handleSplashComplete());
       case Screen.welcome:
         return const WelcomeScreen();
+      case Screen.roleSelection:
+        return RoleSelectionScreen(onSelectRole: (role) => appState.handleRoleSelection(role));
+
       case Screen.register:
-        return RegisterPlaceholder(onNavigateToLogin: () => appState.handleNavigateToLogin());
+        return RegisterScreen(onNavigateToLogin: () => appState.handleNavigateToLogin());
       case Screen.login:
-        return LoginScreen(onNavigateToRegister: () => appState.handleNavigateToRegister());
+        return LoginScreen(onNavigateToRegister: () => appState.handleNavigateToRegister(), selectedRole: appState.userRole);
       case Screen.profile:
         return ProfileScreen(onBack: () => appState.handleBackFromProfile(), onLogout: () => appState.handleLogout(), role: appState.userRole);
       case Screen.requestDetails:
         return RequestDetails(onBack: () => appState.handleBackFromDetails());
+      case Screen.donationDetails:
+        return DonationDetailsScreen(onBack: () => appState.handleBackFromDetails());
+      case Screen.map:
+        return MapScreen(onBack: () => appState.handleBackFromDetails());
       case Screen.campAdminDashboard:
         return CampAdminDashboard(onNavigateToProfile: () => appState.handleNavigateToProfile());
       case Screen.victimDashboard:
-        return VictimDashboard(onNavigateToProfile: () => appState.handleNavigateToProfile());
+        return VictimDashboard(
+          onNavigateToMap: () => appState.handleNavigateToMap(),
+          onNavigateToProfile: () => appState.handleNavigateToProfile(),
+          onNavigateToRequestDetails: () => appState.handleNavigateToRequestDetails(),
+        );
       case Screen.volunteerDashboard:
-        return VolunteerDashboard(onNavigateToProfile: () => appState.handleNavigateToProfile());
+        return VolunteerDashboard(
+          onNavigateToProfile: () => appState.handleNavigateToProfile(),
+          onNavigateToRequestDetails: () => appState.handleNavigateToRequestDetails(),
+          onNavigateToMap: () => appState.handleNavigateToMap(),
+        );
       case Screen.donorDashboard:
-        return DonorDashboard(onNavigateToProfile: () => appState.handleNavigateToProfile());
+        return DonorDashboard(
+          onNavigateToMap: () => appState.handleNavigateToMap(),
+          onNavigateToProfile: () => appState.handleNavigateToProfile(),
+          onNavigateToDonationDetails: () => appState.handleNavigateToDonationDetails(),
+        );
       case Screen.adminDashboard:
         return AdminDashboard(onNavigateToProfile: () => appState.handleNavigateToProfile());
       default:
